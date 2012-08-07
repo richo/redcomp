2e644072d1a0a6260673db4df643ae1594068078
Rich Healey <richard.healey@99designs.com>
2012-08-07 11:34:10 +1000

    [wip] Start refactoring

--- redcomp.py
+++ redcomp.py
@@ -2,11 +2,90 @@
 
 ##Here's the first wack at the redcode compiler.  this will be dirty
 #samurai@psych0tik.net
-#2012-08-06 
+#2012-08-06
 
 
 ##this is mostly set ups tuff
 import sys
+
+_debug = True
+def debug(line):
+    if _debug:
+        sys.stderr.write("%s\n" % (line))
+
+class CodeObject(object):
+    def __init__(self):
+        self.codes = []
+
+    def validate(self):
+        error = 0
+        bracket_open = bracket_close = 0
+        starter = 0
+        for code in self.codes:
+            ##make sure brackets exist all over
+            if "{" in code:
+                bracket_open += 1
+            if "}" in code:
+                bracket_close += 1
+            ##main function, called start() - perhaps should change this to main() to confuse less people?
+            if "start()" in code:
+                starter = 1
+
+        ##less than useful errors
+        if bracket_open != bracket_close:
+            print "[Fatal] Un-matched brackets"
+            error = 1
+        if starter != 1:
+            print "[Fatal] Please provide a start() function"
+            error = 1
+        return error
+    def digest(self, code):
+        """\
+digests some code from `code`, which should be enumerable to fetch code objects
+"""
+    for line in code:
+        line = line.strip()
+        for inst in line.split(";"):
+            if inst != "":
+                debug("D: %s" % (inst))
+                self.codes.append(inst)
+
+    @staticmethod
+    def buildFunc(inst):
+        funccheck = "func %s()" % (funcname) ##need a way to find our start
+        print funccheck
+        foundfunc = bracketstatus = 0
+        funccode = []
+        ##look through for the func name, when finding brackets use the code in between.
+        ##the order of the brackets with the other checks is important for getting the right lines
+        for line in code:
+            if funccheck in line:
+                foundfunc = 1
+            if "}" in line:
+                bracketstatus = 0
+            print bracketstatus
+            print foundfunc
+            if foundfunc and bracketstatus:
+                funccode.append(line)
+            if "{" in line:
+                bracketstatus = 1
+        print funccode
+        ##empty function
+        if len(funccode) == 0:
+            writeCode("%s: dat #0, #0" %  (funcname))
+        ## convert code from rcc to redcode (this probably will be hard?)
+        ##then drop it into the file, built as a "function"
+        else:
+            redcode = convertCode(funccode)
+            print redcode
+            redcode[0] = "%s: %s" % ( funcname, redcode[0] )
+            writeCode(redcode[0])
+            del(redcode[0])
+            for rline in redcode:
+                writeCode("\t%s" % ( rline ))
+
+    def write(self, fh):
+        pass
 
 filename =  sys.argv[1]
 fh = open(filename, "r")
@@ -15,72 +94,15 @@ outfilename = filename.split(".")
 outfilename = outfilename[0]+".red"
 outfile = open(outfilename, "w")
 
-codes = []
-
 ##functions - these will probably be moved to a lib later
 
-##checks to see if the structure of the code looks proper-ish?
-def validateCode(codes):
-	error = 0
-	bracket_open = bracket_close = 0
-	starter = 0
-	for code in codes:
-		##make sure brackets exist all over
-		if "{" in code:
-			bracket_open += 1
-		if "}" in code:
-			bracket_close += 1
-		##main function, called start() - perhaps should change this to main() to confuse less people?
-		if "start()" in code:
-			starter = 1
-		
-	##less than useful errors 
-	if bracket_open != bracket_close:
-		print "[Fatal] Un-matched brackets"
-		error = 1
-	if starter != 1:
-		print "[Fatal] Please provide a start() function"
-		error = 1
-	return error
 
 ##NOTE: thinking about it, functions and variables will be set-up almost the same way within the redcode
-## the only real difference being single vs multi-line.  This may mean that we can use functions/vars interchangably at the higher 
+## the only real difference being single vs multi-line.  This may mean that we can use functions/vars interchangably at the higher
 ## level?
 
 ##this takes the code and a function name and will find/build it in redcode
 def buildFunction(code, funcname):
-	funccheck = "func %s()" % (funcname) ##need a way to find our start
-	print funccheck
-	foundfunc = bracketstatus = 0
-	funccode = []
-	##look through for the func name, when finding brackets use the code in between.
-	##the order of the brackets with the other checks is important for getting the right lines
-	for line in code:
-		if funccheck in line:
-			foundfunc = 1
-		if "}" in line:
-			bracketstatus = 0
-		print bracketstatus
-		print foundfunc
-		if foundfunc and bracketstatus:
-			funccode.append(line)
-		if "{" in line:
-			bracketstatus = 1
-
-	print funccode	
-	##empty function 	
-	if len(funccode) == 0:
-		writeCode("%s: dat #0, #0" %  (funcname))
-	## convert code from rcc to redcode (this probably will be hard?)
-	##then drop it into the file, built as a "function"
-	else:
-		redcode = convertCode(funccode)
-		print redcode
-		redcode[0] = "%s: %s" % ( funcname, redcode[0] )
-		writeCode(redcode[0])
-		del(redcode[0])
-		for rline in redcode:
-			writeCode("\t%s" % ( rline ))	
 
 ##magic happens here
 def convertCode(code):
@@ -93,14 +115,6 @@ def writeCode(redcode):
 
 
 ##get the code we need to compile, skipping comments
-for line in fh:
-	line = line.strip()
-	tmp = line.split(";")
-	line = tmp[0]
-	if line != "":
-		print line
-		codes.append(line)
-
 ##check code for syntax errors
 if validateCode(codes) != 0:
 	print "This code won't compile, please fix your errors"
